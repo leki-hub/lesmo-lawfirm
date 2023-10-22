351026042e7d22f125863a4caca89e0b510c1112
Leki <lekisintole82@gmail.com>
2023-10-23 00:28:22 +0300

    attorneys header

--- src/pages/Attorneys.jsx
+++ src/pages/Attorneys.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Appointment from '../components/Appointment';
 
 const Attorneys = () => {
   const settings = {
@@ -25,6 +26,21 @@ const Attorneys = () => {
   };
 
   return (
+    <>
+        {/* <!-- Page Header Start --> */}
+    <div className="container-fluid bg-page-header" style={{marginBottom: "90px"}}>
+        <div className="container">
+            <div className="d-flex flex-column align-items-center justify-content-center" style={{minHeight: "400px"}}>
+                <h3 className="display-3 text-white text-uppercase">Attorney</h3>
+                <div className="d-inline-flex text-white">
+                    <p className="m-0 text-uppercase"><a className="text-white" href="">Home</a></p>
+                    <i className="fa fa-angle-double-right pt-1 px-3"></i>
+                    <p className="m-0 text-uppercase">Attorney</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    {/* <!-- Page Header End --> */}
     <div className="container-fluid py-5">
       <div className="container py-5">
         <div className="text-center pb-2">
@@ -48,7 +64,9 @@ const Attorneys = () => {
           ))}
         </Slider>
       </div>
+      <Appointment />
     </div>
+    </>
   );
 };
 
