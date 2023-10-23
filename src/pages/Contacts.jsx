0d45c4eb7e31f2c0830d68a7246747eab435822f
Leki <lekisintole82@gmail.com>
2023-10-23 15:09:02 +0300

    form updated

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,114 +1,67 @@
-import React from "react";
+import React from 'react'
 
 const Contacts = () => {
   return (
     <div>
-
-      {/* <!-- Page Header Start --> */}
-      <div
-        className="container-fluid bg-page-header"
-        style={{ marginBottom: "90px" }}
-      >
+    {/* <!-- Page Header Start --> */} 
+    <div className="container-fluid bg-page-header" style={{marginBottom: "90px"}}>
         <div className="container">
-          <div
-            className="d-flex flex-column align-items-center justify-content-center"
-            style={{ minHeight: "400px" }}
-          >
-            <h3 className="display-3 text-white text-uppercase">Contact</h3>
-            <div className="d-inline-flex text-white">
-              <p className="m-0 text-uppercase">
-                <a className="text-white" href="">
-                  Home
-                </a>
-              </p>
-              <i className="fa fa-angle-double-right pt-1 px-3"></i>
-              <p className="m-0 text-uppercase">Contact</p>
+            <div className="d-flex flex-column align-items-center justify-content-center" style={{minHeight: "400px"}}>
+                <h3 className="display-3 text-white text-uppercase">Contact</h3>
+                <div className="d-inline-flex text-white">
+                    <p className="m-0 text-uppercase"><a className="text-white" href="">Home</a></p>
+                    <i className="fa fa-angle-double-right pt-1 px-3"></i>
+                    <p className="m-0 text-uppercase">Contact</p>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      {/* <!-- Page Header End --> */}
-
-      {/* <!-- Contact Start --> */}
-      <div className="container-fluid py-5">
-        <div className="container py-5">
-          <div className="text-center pb-2">
-            <h6 className="text-uppercase">Contact Us</h6>
-            <h1 className="mb-4">Contact For Any Query</h1>
-          </div>
-          <div className="row">
-            <div className="col-lg-6 mb-5 mb-lg-0">
-              <div className="contact-form">
-                <div id="success"></div>
-                <form
-                  name="sentMessage"
-                  id="contactForm"
-                  noValidate="novalidate"
-                >
-                  <div className="form-row">
-                    <div className="col-sm-6 control-group">
-                      <input
-                        type="text"
-                        className="form-control p-4"
-                        id="name"
-                        placeholder="Your Name"
-                        required="required"
-                        data-validation-required-message="Please enter your name"
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                    <div className="col-sm-6 control-group">
-                      <input
-                        type="email"
-                        className="form-control p-4"
-                        id="email"
-                        placeholder="Your Email"
-                        required="required"
-                        data-validation-required-message="Please enter your email"
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="control-group">
-                    <input
-                      type="text"
-                      className="form-control p-4"
-                      id="subject"
-                      placeholder="Subject"
-                      required="required"
-                      data-validation-required-message="Please enter a subject"
-                    />
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div className="control-group">
-                    <textarea
-                      className="form-control p-4"
-                      rows="6"
-                      id="message"
-                      placeholder="Message"
-                      required="required"
-                      data-validation-required-message="Please enter your message"
-                    ></textarea>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div>
-                    <button
-                      className="btn btn-primary btn-block"
-                      type="submit"
-                      id="sendMessageButton"
-                    >
-                      Send Message
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- Contact End --> */}
     </div>
-  );
-};
+   {/* <!-- Page Header End --> */}
 
-export default Contacts;
+    {/* <!-- Contact Start --> */}
+    <div className="container-fluid py-5">
+        <div className="container py-5">
+            <div className="text-center pb-2">
+                <h6 className="text-uppercase">Contact Us</h6>
+                <h1 className="mb-4">Contact For Any Query</h1>
+            </div>
+            <div className="row">
+                <div className="col-lg-6 mb-5 mb-lg-0">
+                    <div className="contact-form">
+                        <div id="success"></div>
+                        <form name="sentMessage" id="contactForm" validate="validate">
+                            <div className="form-row">
+                                <div className="col-sm-6 control-group">
+                                    <input type="text" className="form-control p-4" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                    <p className="help-block text-danger"></p>
+                                </div>
+                                <div className="col-sm-6 control-group">
+                                    <input type="email" className="form-control p-4" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                    <p className="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div className="control-group">
+                                <input type="text" className="form-control p-4" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="control-group">
+                                <textarea className="form-control p-4" rows="6" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div>
+                                <button className="btn btn-primary btn-block" type="submit" id="sendMessageButton">Send Message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {/* <!-- Contact End --> */}
+
+      
+    </div>
+  )
+}
+
+export default Contacts
