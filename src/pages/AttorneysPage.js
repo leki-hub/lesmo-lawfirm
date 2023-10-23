d0ecdb242cafc6a8bd491098ff26a0c76a641435
Leki <lekisintole82@gmail.com>
2023-10-23 08:13:25 +0300

    Attorneys page

---
+++ src/pages/AttorneysPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AttorneyHeader from '../components/attorneys/AttorneyHeader'
+import Attorneys from '../components/attorneys/Attorneys'
+
+const AttorneysPage = () => {
+  return (
+    <div>
+        <AttorneyHeader />
+        <Attorneys />
+      
+    </div>
+  )
+}
+
+export default AttorneysPage
