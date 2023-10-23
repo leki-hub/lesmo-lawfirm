62392b0e17072c50ad55cd163a8e382f48046301
Leki <lekisintole82@gmail.com>
2023-10-23 13:24:22 +0300

    responsive testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -8,22 +9,36 @@ const Testimonial = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3, // Show three items at a time
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 992, // Medium screens
+        settings: {
+          slidesToShow: 2, // Show two items at a time
+        }
+      },
+      {
+        breakpoint: 768, // Small screens
+        settings: {
+          slidesToShow: 1, // Show one item at a time
+        }
+      }
+    ]
   };
 
   return (
     <div className="container-fluid py-5">
-      <div className="container py-5">
+     <div className="container py-5">
         <div className="text-center pb-5">
           <h6 className="text-uppercase">Testimonial</h6>
           <h1 className="mb-5">What Our Clients Say</h1>
         </div>
         <Slider {...settings} className="testimonial-carousel">
           <div className="testimonial-item">
-            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 mb-4">
+            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
               Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
             </div>
             <div className="d-flex align-items-center pt-3">
@@ -35,7 +50,7 @@ const Testimonial = () => {
             </div>
           </div>
           <div className="testimonial-item">
-            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 mb-4">
+            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
               Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
             </div>
             <div className="d-flex align-items-center pt-3">
@@ -47,7 +62,7 @@ const Testimonial = () => {
             </div>
           </div>
           <div className="testimonial-item">
-            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 mb-4">
+            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
               Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
             </div>
             <div className="d-flex align-items-center pt-3">
@@ -59,7 +74,7 @@ const Testimonial = () => {
             </div>
           </div>
           <div className="testimonial-item">
-            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 mb-4">
+            <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
               Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
             </div>
             <div className="d-flex align-items-center pt-3">
@@ -77,4 +92,3 @@ const Testimonial = () => {
 };
 
 export default Testimonial;
-
