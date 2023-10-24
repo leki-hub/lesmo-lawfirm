0c4aed2637a5c44e12ee324466d179c8e68fae14
Leki <lekisintole82@gmail.com>
2023-10-24 12:00:34 +0300

    autoplay

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -39,49 +39,53 @@ const Testimonial = () => {
         <Slider {...settings} className="testimonial-carousel">
           <div className="testimonial-item">
             <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
-              Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
+            "I was impressed by the expertise and professionalism of the team at Lesmo Advocates.
+            Their dedication to our case was evident, and we're grateful for their outstanding service.". 
             </div>
             <div className="d-flex align-items-center pt-3">
               <img className="img-fluid rounded-circle" src="img/testimonial-1.jpg" style={{width: "80px", height: "80px"}} alt="" />
               <div className="pl-4">
-                <h5>Client Name</h5>
-                <p className="m-0">Profession</p>
+                <h5>Bill</h5>
+                <p className="m-0">Farmer</p>
               </div>
             </div>
           </div>
           <div className="testimonial-item">
             <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
-              Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
+            "I hired Lesmo Advocates for a property dispute, and they exceeded my expectations. 
+            Their attention to detail and strategic approach were instrumental in resolving the issue efficiently. 
             </div>
             <div className="d-flex align-items-center pt-3">
               <img className="img-fluid rounded-circle" src="img/testimonial-2.jpg" style={{width: "80px", height: "80px"}} alt="" />
               <div className="pl-4">
-                <h5>Client Name</h5>
-                <p className="m-0">Profession</p>
+                <h5>Gates</h5>
+                <p className="m-0">Doctor</p>
               </div>
             </div>
           </div>
           <div className="testimonial-item">
             <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
-              Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
+            "The team at Lesmo Advoacates handled our family's estate planning with care and expertise. 
+            They took the time to understand our unique needs and provided comprehensive guidance throughout the process". 
             </div>
             <div className="d-flex align-items-center pt-3">
               <img className="img-fluid rounded-circle" src="img/testimonial-3.jpg" style={{width: "80px", height: "80px"}} alt="" />
               <div className="pl-4">
-                <h5>Client Name</h5>
-                <p className="m-0">Profession</p>
+                <h5>Nicola</h5>
+                <p className="m-0">Real Estate</p>
               </div>
             </div>
           </div>
           <div className="testimonial-item">
             <div className="testimonial-text position-relative bg-secondary text-light rounded p-5 m-4">
-              Sed ea amet kasd elitr stet nonumy, stet rebum et ipsum est duo elitr clita lorem
+            "When I needed criminal defense, Lesmo Advocates provided unwavering support and expert representation. 
+            Their team navigated the legal process with skill and dedication, resulting in a positive outcome for my case."
             </div>
             <div className="d-flex align-items-center pt-3">
               <img className="img-fluid rounded-circle" src="img/testimonial-4.jpg" style={{width: "80px", height: "80px"}} alt="" />
               <div className="pl-4">
-                <h5>Client Name</h5>
-                <p className="m-0">Profession</p>
+                <h5>Warren</h5>
+                <p className="m-0">Entrepreneur</p>
               </div>
             </div>
           </div>
